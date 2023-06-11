0847e3521b0fcb58add8f2b33cfaa74d1b26fa32
im-Arn <110503940+im-Arn@users.noreply.github.com>
2023-06-11 15:47:04 +0300

    init

---
+++ src/app/layout.jsx
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Агросфера',
+  description: 'Ваш надежный поставщик укрывных материалов',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body suppressHydrationWarning={true} className="font-text min-h-screen">{children}</body>
+    </html>
+  )
+}
