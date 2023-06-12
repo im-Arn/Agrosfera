dfc55601d0c7faf5a4b24fda4a8fcc1e8e9abe66
im-Arn <110503940+im-Arn@users.noreply.github.com>
2023-06-12 22:49:13 +0300

    add MainProducts

---
+++ src/components/MainPage/MainProducts/Accordion/index.jsx
@@ -0,0 +1,46 @@
+'use client'
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { arrowB, arrowG } from '../../../../assets/images/Accordion';
+
+const Accordion = ({ title, subtitle, href, img }, key) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <li className='w-full pb-[23px] lg:pb-[10px] flex flex-col bg-[#ffffff]' key={key}>
+      <button className={`w-full h-[56px] lg:h-[60px] ${isOpen ? 'lg:h-[55px]' : ''} md:pr-[30px] 
+      flex justify-between items-end 
+      transition-all duration-300 ease-linear`} onClick={toggleAccordion}>
+        <h3 className={`text-[16px] heading-[18px] md:text-[24px] md:leading-[24px] ${isOpen ? 'text-[#000000]' : 'text-[#898989]'} transition-all duration-75 ease-linear`}>{title}</h3>
+        {!isOpen
+          ? <Image
+            className='w-[18px] h-[18px] md:w-[27px] md:h-[27px] cursor-pointer'
+            src={arrowG}
+            alt='иконка стрелки'
+          />
+          : <Image
+            className='w-[18px] h-[18px] md:w-[27px] md:h-[27px] cursor-pointer'
+            src={arrowB}
+            alt='иконка стрелки'
+          />}
+      </button>
+      <div className={`max-h-[0px] pt-[10px] lg:mb-[15px] lg:pt-[15px] flex flex-col gap-[10px] md:gap-[20px] overflow-hidden transition-all duration-300 ease-linear 
+      text-white ${isOpen ? 'max-h-[400px]' : ''}`}>
+        <p className='lg:mb-[10px] text-[14px] leading-[17px] md:text-[16px] md:leading-[21px] font-normal text-[#2E2E2E]'>{subtitle}</p>
+        <div className='w-full lg:hidden pb-[13px]'>
+          <Image
+            className='object-contain w-full max-h-[300px] rounded'
+            src={img}
+            alt={`изображение ${title}`}
+          />
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default Accordion;
