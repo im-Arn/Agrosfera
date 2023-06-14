62beb06e0702261e8dacb46e13242ff86ed336f7
im-Arn <110503940+im-Arn@users.noreply.github.com>
2023-06-14 06:42:57 +0300

    fix key

--- src/components/Header/HeaderTop/TopItem/index.jsx
+++ src/components/Header/HeaderTop/TopItem/index.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-const TopItem = ({ link, children, key }) => {
+const TopItem = ({ link, children }, key) => {
 
   return (
     <li className='flex items-center font-normal text-[12px] leading-[12px] md:text-[14px] md:leading-[14px]' key={key}><Link href={link} className='flex items-center'>{children}</Link></li>
