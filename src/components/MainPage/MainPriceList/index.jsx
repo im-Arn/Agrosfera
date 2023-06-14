a4c9ecbd16cf6c40a19f1c38b690d6942cbd8fec
im-Arn <110503940+im-Arn@users.noreply.github.com>
2023-06-14 08:21:58 +0300

    add new form

--- src/components/MainPage/MainPriceList/index.jsx
+++ src/components/MainPage/MainPriceList/index.jsx
@@ -3,23 +3,84 @@ import { Title2, Button } from '../../ui';
 import React, { useState } from 'react';
 
 const MainPriceList = () => {
-  const [error, setError] = useState(false);
-  const [phone, setPhone] = useState('');
+
+  const [formValues, setFormValues] = useState({
+    person: '',
+    mail: '',
+    phone: '',
+    message: '',
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    name: false,
+    mail: false,
+    phone: false,
+  });
 
   const handleChange = (e) => {
-    const inputValue = e.target.value;
-    setPhone(inputValue);
-    if (!/^\+?\d+$/.test(inputValue)) {
-      setError(true);
-    } else {
-      setError(false);
+    const { name, value } = e.target;
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+    validateField(name, value);
+  };
+
+  const validateField = (fieldName, value) => {
+    let error = false;
+
+    // правила валидации для каждого поля
+    if (fieldName === 'person') {
+      if (value.trim() === '') {
+        error = true;
+      } else {
+        error = false;
+      }
+    } else if (fieldName === 'mail') {
+      if (!/\S+@\S+\.\S+/.test(value)) {
+        error = true;
+      } else {
+        error = false;
+      }
+    } else if (fieldName === 'phone') {
+      if (!/^\+?\d+$/.test(value)) {
+        error = true;
+      } else {
+        error = false;
+      }
     }
+
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      [fieldName]: error,
+    }));
+  };
+
+  const isFormValid = () => {
+    return Object.values(formErrors).every((error) => error === false);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setPhone('');
-    setError(false);
+
+    if (isFormValid()) {
+      setFormValues((prevValues) => ({
+        ...prevValues,
+        person: '',
+        mail: '',
+        phone: '',
+      }));
+    } else {
+      console.log('Форма содержит ошибки');
+      setFormErrors((prevErrors) => ({
+        ...prevErrors,
+        person: true,
+        mail: true,
+        phone: true,
+      }));
+    }
+
+
   };
 
   return (
@@ -29,16 +90,64 @@ const MainPriceList = () => {
   absolute top-[126px] md:top-[30px] md:right-40 px-[15px] py-[30px] md:px-[39px]'>
         <Title2>Получить прайс-лист</Title2>
         <form className='mt-[30px] flex flex-col items-center gap-[15px]' onSubmit={handleSubmit}>
-          <label className='w-full flex flex-col gap-[10px] text-[16px] leading-[16px] font-medium'><p>Ваш телефон</p>
+          <label className='w-full flex flex-col gap-[10px] text-[16px] leading-[16px] font-medium' htmlFor='person'><p>Название компании</p>
             <input
-              type='tel'
-              placeholder='+7 (492)'
-              value={phone}
+              type='text'
+              id='person'
+              name='person'
+              placeholder='ИЛИ ВАШЕ ИМЯ'
+              value={formValues.person}
               maxLength='12'
               onChange={handleChange}
               className={`w-full max-w-[310px] md:max-w-full h-[50px] px-[25px] rounded-md border-[1px] bg-transparent 
             font-normal text-[14px] leading-[14px] focus:outline-none focus:border-black
-           ${error ? 'border-[#DD4433] text-[#DD4433] focus:border-[#DD4433]' : 'border-[#C4C4C4]'}  
+           ${formErrors.person ? 'border-[#DD4433] text-[#DD4433] focus:border-[#DD4433]' : 'border-[#C4C4C4]'}  
+           placeholder:text-[#898989] placeholder:text-[14px] placeholder:leading-[14px]
+           `}
+            />
+          </label>
+          <label className='w-full flex flex-col gap-[10px] text-[16px] leading-[16px] font-medium' htmlFor='mail'><p>Ваш email</p>
+            <input
+              type='email'
+              id='mail'
+              name='mail'
+              placeholder='mail@mail.mail'
+              value={formValues.mail}
+              maxLength='50'
+              onChange={handleChange}
+              className={`w-full max-w-[310px] md:max-w-full h-[50px] px-[25px] rounded-md border-[1px] bg-transparent 
+            font-normal text-[14px] leading-[14px] focus:outline-none focus:border-black
+           ${formErrors.mail ? 'border-[#DD4433] text-[#DD4433] focus:border-[#DD4433]' : 'border-[#C4C4C4]'}  
+           placeholder:text-[#898989] placeholder:text-[14px] placeholder:leading-[14px]
+           `}
+            />
+          </label>
+          <label className='w-full flex flex-col gap-[10px] text-[16px] leading-[16px] font-medium' htmlFor='phone'><p>Ваш телефон</p>
+            <input
+              type='tel'
+              id='phone'
+              name='phone'
+              placeholder='+7 (492)'
+              value={formValues.phone}
+              maxLength='12'
+              minLength='10'
+              onChange={handleChange}
+              className={`w-full max-w-[310px] md:max-w-full h-[50px] px-[25px] rounded-md border-[1px] bg-transparent 
+            font-normal text-[14px] leading-[14px] focus:outline-none focus:border-black
+           ${formErrors.phone ? 'border-[#DD4433] text-[#DD4433] focus:border-[#DD4433]' : 'border-[#C4C4C4]'}  
+           placeholder:text-[#898989] placeholder:text-[14px] placeholder:leading-[14px]
+           `}
+            />
+          </label>
+          <label className='w-full flex flex-col gap-[10px] text-[16px] leading-[16px] font-medium' htmlFor='message'><p>Ваш телефон</p>
+            <textarea
+              id='message'
+              name='message'
+              placeholder=''
+              value={formValues.message}
+              onChange={handleChange}
+              className={`w-full max-w-[310px] md:max-w-full h-[123px] resize-none px-[25px] py-[20px] rounded-md border-[1px] bg-transparent 
+            font-normal text-[14px] leading-[14px] focus:outline-none focus:border-black  
            placeholder:text-[#898989] placeholder:text-[14px] placeholder:leading-[14px]
            `}
             />
